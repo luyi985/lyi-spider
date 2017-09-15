f140da6c271fe2626ce89248c97b2cc6dca52fc3
Yi Lu <yilu@192-168-1-6.tpgi.com.au>
2017-09-16 00:16:39 +1000

    get DATA

--- app/testMedware.js
+++ app/testMedware.js
@@ -1,4 +1,6 @@
 import express from "express";
+import request from 'request';
+import cheerio from 'cheerio';
 
 const testRouter = express.Router();
 
@@ -7,8 +9,19 @@ testRouter.use((req, res, next) => {
 })
 
 testRouter.get('/:name', (req, res) => {
-	console.log("send back response")
-	res.send("test medware...")
+	request(config.test.url, (error, response, html) => {
+		let message = [];
+  		if (!error && response.statusCode == 200) {
+  			const $ = cheerio.load(html);
+		    $(config.test.selector).each((i, element) => {
+		      // message.push({
+		      // 	bankName: $(element).find('td.company img').attr('alt'),
+		      // 	package: $(element).find('td.productName span.product-title').text()
+		      // });
+		    });
+  		}
+  		res.send(JSON.stringify(message));
+	});
 })
 
 testRouter.param('name', (req, res, next) => {
@@ -22,4 +35,36 @@ testRouter.param('name', (req, res, next) => {
 	}
 })
 
+function percentToFload (percent) {
+	return parseFloat(percent.slice(0, -1));
+}
+
+function dollarToInt (dollar) {
+	const v = dollar.replace(',','');
+	return parseInt(v.slice(1));
+}
+
+testRouter.get('/', (req, res) => {
+	request(config.test.url, (error, response, html) => {
+		let message = [];
+  		if (!error && response.statusCode == 200) {
+  			const $ = cheerio.load(html);
+		    $(config.test.selector).each((i, element) => {
+		    	message.push({
+		       		bankName: $(element).find('td.company img').attr('alt'),
+		       		package: $(element).find('td.productName span.product-title').text(),
+		       		adRate: {
+		       			rate: percentToFload($(element).find('td.advertisedRate .cell-value').text()),
+		       			detail: $(element).find('td.advertisedRate .cell-details').text()
+		       		},
+		       		compareRate: percentToFload($(element).find('td.comparisonRate .cell-value').text()),
+		       		repayment: dollarToInt($(element).find('td.monthlyRepayment .cell-value').text()),
+		       		upFrontFee: dollarToInt($(element).find('td.upFrontFee .cell-value').text()),
+		       		url: $(element).find('td.apply .gotosite').attr('href')
+		    	});
+		    });
+  		}
+  		res.send(JSON.stringify(message));
+	});
+})
 export default testRouter;
---
+++ config.js
@@ -0,0 +1,6 @@
+global.config = {
+	test: {
+		url: "http://www.ratecity.com.au/home-loans/compare?h_bank=true&h_flexibilityScore=23&h_maxBorrowingAmount=744000&h_minDeposit=26&h_nswApplicable=true&h_ownerOccupied=true&h_per_page=2000&h_principalAndInterest=true&h_propertyValue=1000000&ab_test_id=0.52",
+		selector: ".rate-table.table.table-hover tr.product"// "
+	}
+}
