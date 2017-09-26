4e81a955f632c982a0ff88b46fb654c0ed90e2b1
Yi Lu <yilu@Yis-MacBook-Pro.local>
2017-09-26 13:18:31 +1000

    slack message

--- src/app/slackMiddleware.js
+++ src/app/slackMiddleware.js
@@ -7,7 +7,8 @@ const clientSecret = '6df288cf5c5db6ab6338959422461cc6';
 const scope = 'incoming-webhook,chat:write:bot'
 const redirectUri = 'http://138.197.9.69:8080/slack/access';//'http://localhost:8080/slack/access';
 const code = "237550111488.242831798641.efb15339cb861de67c51ec52e14614454a1a593fac84232b3adee4c8e4b4f603"
-
+const token = "xoxp-237550111488-237682550961-243737065895-541b4138af290fd4e75f16c1d5786679";
+const webhook = "https://hooks.slack.com/services/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";
 const auth = [
 	`client_id=${clientId}`,
 	`scope=${scope}`,
@@ -53,6 +54,27 @@ slackRouter.get('/access', (req, res) => {
 	.catch(e => {
 		res.send(e);
 	})
+})
+
+slackRouter.get('/msg', (req, res) => {
+	const text = req.query.t;
+	q({
+		method: 'POST',
+		url: webhook,
+		body: JSON.stringify({
+			text: text
+		}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then(body => {
+		res.send(body)
+	})
+	.catch(e => {
+		res.send(e);
+	})
+
 })
 
 export default slackRouter;
